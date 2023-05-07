6f200402b871cc0c52d615bf2a4a52bd5f46ddb8
Rebecca Lehew <lehew316@gmail.com>
2023-05-07 15:29:38 -0500

    Update connection.js
    Removed Sequelize syntax and replaced with mySQL syntax.

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,12 @@
-const Sequelize = require('sequelize');
 require('dotenv').config();
+const mysql = require('mysql2');
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: '127.0.0.1',
-    dialect: 'mysql',
-    port: 3306,
-  }
-);
+// Database connection
+const bookstore = mysql.createConnection({
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.HOST,
+  database: process.env.DB_NAME,
+});
 
-module.exports = sequelize;
+module.exports = bookstore;
